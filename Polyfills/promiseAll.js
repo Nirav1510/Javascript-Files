a2d9b4fa9f211b4ee72c8a190e768d6270e7991b
Nirav Gusai <gusainirav1@gmail.com>
2024-01-27 19:20:10 +0530

    polyfill of Promise.AllSettled()

--- Polyfills/promiseAll.js
+++ Polyfills/promiseAll.js
@@ -38,22 +38,26 @@ function myPromiseAll(taskList) {
     // });
   });
 }
-function task(time) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      if (time < 3000) {
-        reject("Rejected");
-      } else {
-        resolve(time);
-      }
+const resolvedApis = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(time);
     }, time);
   });
-}
+};
 
-const taskList = [task(7000), task(5000), task(4000)];
+const rejectedApis = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(time);
+    }, time);
+  });
+};
+
+let taskList = [resolvedApis(500), resolvedApis(2000), rejectedApis(1000)];
 
 myPromiseAll(taskList)
   .then((results) => {
     console.log("got results", results);
   })
-  .catch(console.error);
+  .catch((e) => console.error("error", e));
---
+++ Polyfills/promiseAllSettled.js
@@ -0,0 +1,48 @@
+const resolvedApis = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(time);
+    }, time);
+  });
+};
+
+const rejectedApis = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(time);
+    }, time);
+  });
+};
+
+let taskList = [resolvedApis(500), resolvedApis(2000), rejectedApis(1000)];
+
+function myPromiseAllSettled(taskList) {
+  const results = [];
+  let promisesCompleted = 0;
+
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < taskList.length; i++) {
+      taskList[i]
+        .then((res) => {
+          results[i] = { status: "fulfilled", value: res };
+          promisesCompleted += 1;
+          if (promisesCompleted === taskList.length) {
+            resolve(results);
+          }
+        })
+        .catch((e) => {
+          results[i] = { status: "rejected", reason: e };
+          promisesCompleted += 1;
+          if (promisesCompleted === taskList.length) {
+            reject(results);
+          }
+        });
+    }
+  });
+}
+
+myPromiseAllSettled(taskList)
+  .then((results) => {
+    console.log("got results", results);
+  })
+  .catch((e) => console.error("error", e));
