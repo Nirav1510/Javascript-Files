f905c44f58b9191f76a681570e5adc20e0e67d7f
Nirav Gusai <gusainirav1@gmail.com>
2024-01-27 19:39:20 +0530

    polyfill of Promise.Any()

---
+++ Polyfills/promiseAny.js
@@ -0,0 +1,43 @@
+const resolvedApis = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(time);
+    }, time);
+  });
+};
+
+const rejectedApis = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(time);
+    }, time);
+  });
+};
+
+let taskList = [rejectedApis(500), rejectedApis(1500), rejectedApis(1000)];
+
+function myPromiseAny(taskList) {
+  let result = "";
+  let count = 0;
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < taskList.length; i++) {
+      taskList[i]
+        .then((res) => {
+          result = res;
+          resolve(result);
+        })
+        .catch((err) => {
+          count += 1;
+          if (count === taskList.length) {
+            reject(["Aggregrate Error: All Promises rejected"]);
+          }
+        });
+    }
+  });
+}
+
+myPromiseAny(taskList)
+  .then((results) => {
+    console.log("got results", results);
+  })
+  .catch((e) => console.error("error", e));
