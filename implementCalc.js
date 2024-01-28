46d60acbb9ca68c4479659b20bfc99de553de887
Nirav Gusai <gusainirav1@gmail.com>
2024-01-29 01:13:54 +0530

    implemented calc

---
+++ implementCalc.js
@@ -0,0 +1,18 @@
+const calc = {
+  total: 0,
+  add: function (num) {
+    this.total += num;
+    return this;
+  },
+  multiply: function (num) {
+    this.total *= num;
+    return this;
+  },
+  subtract: function (num) {
+    this.total -= num;
+    return this;
+  },
+};
+
+const result = calc.add(10).multiply(5).subtract(10).add(10);
+console.log(result.total);
