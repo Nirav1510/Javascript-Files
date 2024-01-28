8bc48e9d9c65e2f845910930b2c1b4b1586aad58
Nirav Gusai <gusainirav1@gmail.com>
2024-01-28 12:39:09 +0530

    update

--- implementCurrying.js
+++ implementCurrying.js
@@ -13,9 +13,33 @@ function curry(func) {
 }
 
 const sum = (a, b, c, d, e) => a + b + c + d + e;
-
 const totalSum = curry(sum);
-
 const res = totalSum(1)(2)(3)(5)(9);
-
 console.log(res, "res");
+
+// reusable function
+function evaluate(operation) {
+  return function (a) {
+    return function (b) {
+      switch (operation) {
+        case "add":
+          return a + b;
+
+        case "subtract":
+          return a - b;
+
+        case "multiply":
+          return a * b;
+
+        case "divide":
+          return a / b;
+
+        default:
+          return "invalid operation";
+      }
+    };
+  };
+}
+
+const addition = evaluate("add")(1)(2);
+console.log(addition);
