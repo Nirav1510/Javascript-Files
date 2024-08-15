667d24bc8c1c6290a0f42cde12e8d350cbb1ed6e
Nirav Gusai <gusainirav1@gmail.com>
2024-08-16 02:11:09 +0530

    flatten a nested array

---
+++ Polyfills/arrayFlat.js
@@ -0,0 +1,18 @@
+const flattenArray = (arr) => {
+  let result = [];
+
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      result = [...result, ...flattenArray(item)];
+    } else {
+      result.push(item);
+    }
+  });
+
+  return result;
+};
+
+// Example usage:
+const nestedArray = [1, [2, [3, 4], 5], 6];
+const flattenedArray = flattenArray(nestedArray);
+console.log(flattenedArray); // Output: [1, 2, 3, 4, 5, 6]
