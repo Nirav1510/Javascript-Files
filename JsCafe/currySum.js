22f5d5fbd5cf6c29b1686d46aa3244f1d7a7ef28
Nirav Gusai <gusainirav1@gmail.com>
2024-08-24 23:52:09 +0530

    curryAndSum implementation

---
+++ JsCafe/currySum.js
@@ -0,0 +1,19 @@
+// Write a JavaScript function named curryAndSum that takes five arguments and returns the sum of these arguments.
+// The function should allow passing arguments in any order or fashion.
+
+function curryAndSum(...args) {
+  if (args.length === 5) {
+    return args.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  return function (...newArgs) {
+    return curryAndSum(...args, ...newArgs);
+  };
+}
+
+console.log(curryAndSum(1, 2, 3, 4, 5));
+console.log(curryAndSum(1, 2, 3, 4)(5));
+console.log(curryAndSum(1)(2)(3)(4)(5));
+console.log(curryAndSum(1, 2, 3)(4, 5));
+console.log(curryAndSum(1, 2)(3, 4, 5));
+console.log(curryAndSum(1)(2, 3, 4, 5));
