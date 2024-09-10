cbfb2dce5c4611c61fb6792c3f15c897efb33e15
Nirav Gusai <gusainirav1@gmail.com>
2024-09-10 21:11:26 +0530

    update

--- Polyfills/call.apply.bind.js
+++ Polyfills/call.apply.bind.js
@@ -40,6 +40,6 @@ function purchaseCar(currency, price) {
   console.log(this.color + " " + this.company + " " + currency + " " + price);
 }
 
-// purchaseCar.myCall(car1, "$", 1000000);
+purchaseCar.myCall(car1, "$", 1000000);
 purchaseCar.myApply(car1, ["$", 1000000]);
 purchaseCar.myBind(car1, "$", 1000000);
