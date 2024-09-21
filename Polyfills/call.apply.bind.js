9ed5a8198c87b9cebb846f633a6685c0953e21ee
Nirav Gusai <gusainirav1@gmail.com>
2024-09-21 11:44:00 +0530

    update

--- Polyfills/call.apply.bind.js
+++ Polyfills/call.apply.bind.js
@@ -11,18 +11,6 @@ Function.prototype.myCall = function (context = {}, ...args) {
   context.fn(...args);
 };
 
-Function.prototype.myBind = function (context = {}, ...args) {
-  if (typeof this !== "function") {
-    throw new Error(this + "Error");
-  }
-
-  context.fn = this;
-
-  return function (...newArgs) {
-    return context.fn(...args, ...newArgs);
-  };
-};
-
 Function.prototype.myApply = function (context = {}, args = []) {
   if (typeof this !== "function") {
     throw new Error(this + "Error");
@@ -34,6 +22,18 @@ Function.prototype.myApply = function (context = {}, args = []) {
 
   context.fn = this;
   context.fn(...args);
+};
+
+Function.prototype.myBind = function (context = {}, ...args) {
+  if (typeof this !== "function") {
+    throw new Error(this + "Error");
+  }
+
+  context.fn = this;
+
+  return function (...newArgs) {
+    return context.fn(...args, ...newArgs);
+  };
 };
 
 function purchaseCar(currency, price) {
