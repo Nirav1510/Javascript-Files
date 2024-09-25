0036c438cdc0aa9d932fd59434271392a5e2aea7
Nirav Gusai <gusainirav1@gmail.com>
2024-09-25 09:37:05 +0530

    custom event emitter created

---
+++ script/myEventEmitter.js
@@ -0,0 +1,27 @@
+class EventEmitter {
+  listeners = [];
+  emit(eventName, data) {
+    this.listeners
+      .filter((listener) => listener?.name === eventName)
+      .forEach(({ callback }) =>
+        setTimeout(callback.apply(this, [this, ...data]), 0)
+      );
+  }
+
+  on(name, callback) {
+    if (typeof callback === "function" && typeof name === "string") {
+      this.listeners.push({ name, callback });
+    }
+  }
+
+  off(eventName, callback) {
+    this.listeners = this.listeners.filter(
+      (listener) =>
+        !(listener.name === eventName && listener.callback === callback)
+    );
+  }
+
+  destroy() {
+    this.listener.length = 0;
+  }
+}
