5922145642132a5d5b35a572ad475bca11b35624
Nirav Gusai <gusainirav1@gmail.com>
2024-11-23 20:57:02 +0530

    [Big Frontend] findCorrespondingNode

---
+++ BigFrontEndDev/findCorrespondingNode.js
@@ -0,0 +1,18 @@
+// https://bigfrontend.dev/problem/find-corresponding-node-in-two-identical-DOM-tree
+
+const findCorrespondingNode = (rootA, rootB, target) => {
+  const stackA = [rootA];
+  const stackB = [rootB];
+
+  while (stackA.length) {
+    currentA = stackA.pop();
+    currentB = stackB.pop();
+
+    if (currentA === target) {
+      return currentB;
+    }
+
+    stackA.push(...currentA.children);
+    stackB.push(...currentB.children);
+  }
+};
