2ae2a5351f6380702a17f9915f11005a53dfe536
Nirav Gusai <gusainirav1@gmail.com>
2024-11-25 23:39:39 +0530

    [Big Frontend] removeChars

---
+++ BigFrontEndDev/removeCharacters.js
@@ -0,0 +1,15 @@
+// https://bigfrontend.dev/problem/remove-characters
+
+function removeChars(s) {
+  let stack = [];
+
+  for (char of s) {
+    if (stack.length && char === "c" && stack[stack.length - 1] === "a") {
+      stack.pop();
+    } else if (char !== "b") {
+      stack.push(char);
+    }
+  }
+
+  return stack.join("");
+}
