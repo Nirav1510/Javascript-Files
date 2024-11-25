3ff1938c6f12c2adfca1e2dbc4eebe8ffae8a5f0
Nirav Gusai <gusainirav1@gmail.com>
2024-11-25 23:53:52 +0530

    [Big Frontend] mostFrequentChar

---
+++ BigFrontEndDev/Easy/frequentChar.js
@@ -0,0 +1,19 @@
+// https://bigfrontend.dev/problem/most-frequently-occurring-character
+
+const mostFrequentChar = (str) => {
+  let ans = [];
+  let map = new Map();
+
+  for (const c of str) {
+    map.set(c, (map.get(c) || 0) + 1);
+  }
+
+  const max = Math.max(...map.values());
+
+  for (const [key, value] of map) {
+    if (value === max) {
+      ans.push(key);
+    }
+  }
+  return ans.length === 1 ? ans[0] : ans;
+};
