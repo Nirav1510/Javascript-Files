64f73a250d3b48f72134b46233dd3dd1c00e95ab
Nirav Gusai <gusainirav1@gmail.com>
2024-11-25 22:09:22 +0530

    [Big Frontend] createSum

---
+++ BigFrontEndDev/createSum.js
@@ -0,0 +1,21 @@
+// https://bigfrontend.dev/problem/create-a-sum
+
+function sum(num) {
+  function newSum(num2) {
+    if (num2) {
+      return sum(num + num2);
+    } else {
+      return num;
+    }
+  }
+
+  newSum.valueOf = () => num;
+  console.log(num);
+  return newSum;
+}
+
+const sum1 = sum(1);
+sum1(2) == 3; // true
+sum1(3) == 4; // true
+sum(1)(2)(3) == 6; // true
+sum(5)(-1)(2) == 6; // true
