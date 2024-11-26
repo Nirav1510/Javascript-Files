4f3b1b5a472a6220eb19540495ee889f039648eb
Nirav Gusai <gusainirav1@gmail.com>
2024-11-27 00:37:21 +0530

    [Big Frontend] decodeMessage

---
+++ BigFrontEndDev/Medium/decodeMessage.js
@@ -0,0 +1,31 @@
+// https://bigfrontend.dev/problem/decode-message
+
+function decode(message) {
+  let row = 0,
+    col = 0;
+
+  const colLength = message[0]?.length;
+  let ans = "",
+    step = 1;
+
+  while (col < colLength) {
+    ans = ans + message[row][col];
+
+    if (!message[row + step]) {
+      step = step * -1;
+    }
+
+    row = row + step;
+    col++;
+  }
+  return ans;
+}
+
+const message = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"],
+  ["j", "k", "l"],
+];
+
+console.log(decode(message));
