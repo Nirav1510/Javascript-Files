99edd6b567ca0ac123a1c0323e4717e752fd996a
Nirav Gusai <gusainirav1@gmail.com>
2024-11-26 22:15:46 +0530

    [Big Frontend] firstDuplicate

---
+++ BigFrontEndDev/Easy/firstDuplicateInString.js
@@ -0,0 +1,15 @@
+// https://bigfrontend.dev/problem/find-the-first-duplicate-character-in-a-string
+
+const firstDuplicate = (string) => {
+  let obj = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if (obj[string[i]]) {
+      return string[i];
+    }
+
+    obj[string[i]] = true;
+  }
+
+  return null;
+};
