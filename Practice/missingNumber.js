fbcb159ce850797e7afb3914edf6e626347d8083
Nirav Gusai <gusainirav1@gmail.com>
2024-11-29 22:25:49 +0530

    update

--- Practice/missingNumber.js
+++ Practice/missingNumber.js
@@ -1,10 +1,10 @@
 // https://leetcode.com/problems/missing-number
 
-var missingNumber = function (nums) {
-  const sum = nums.reduce((a, b) => a + b, 0);
+var missingNumber = function (arr) {
+  const sum = arr.reduce((a, b) => a + b, 0);
   let rangeSum = 0;
 
-  for (let i = 0; i <= nums.length; i++) {
+  for (let i = 0; i <= arr.length; i++) {
     rangeSum += i;
   }
 
