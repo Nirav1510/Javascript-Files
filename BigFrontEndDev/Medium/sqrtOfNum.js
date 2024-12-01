9a1d8eeb7a8c3896aa20842b620d5b59ccebee7a
Nirav Gusai <gusainirav1@gmail.com>
2024-12-02 01:19:55 +0530

    [Big Frontend] mySqrt

---
+++ BigFrontEndDev/Medium/sqrtOfNum.js
@@ -0,0 +1,25 @@
+// https://bigfrontend.dev/problem/implement-Math-sqrt
+
+function mySqrt(x) {
+  if (x < 0 || isNaN(x)) {
+    return NaN;
+  }
+  if (x == 1) {
+    return 1;
+  }
+  let low = 1;
+  let high = x / 2;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = mid * mid;
+    if (guess == x) {
+      return mid;
+    }
+    if (guess < x) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return high;
+}
