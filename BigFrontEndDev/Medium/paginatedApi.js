bf7142034e8fc050458c02a3bc81727e56cef854
Nirav Gusai <gusainirav1@gmail.com>
2024-12-02 22:34:13 +0530

    [Big Frontend] paginated api

---
+++ BigFrontEndDev/Medium/paginatedApi.js
@@ -0,0 +1,24 @@
+// https://bigfrontend.dev/problem/call-APIs-with-pagination
+
+// fetchList is provided for you
+// const fetchList = (since?: number) =>
+//   Promise<{items: Array<{id: number}>}>
+
+// you can change this to generator function if you want
+const fetchListWithAmount = async (amount = 5) => {
+  // your code here
+  let result = [],
+    cursor;
+
+  while (result.length < amount) {
+    const { items } = await fetchList(cursor);
+    if (items.length > 0) {
+      result.push(...items);
+      cursor = items[items.length - 1].id;
+    } else {
+      break;
+    }
+  }
+
+  return result.slice(0, amount);
+};
