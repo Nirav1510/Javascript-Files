1bc049229b753d033155cac455e2b1d8d5ffb700
Nirav Gusai <gusainirav1@gmail.com>
2024-12-02 21:03:12 +0530

    update

--- BigFrontEndDev/Medium/suffleArray.js
+++ BigFrontEndDev/Medium/suffleArray.js
@@ -3,9 +3,7 @@
 const fisherYaleShuffleArray = (arr) => {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    const temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
 
   return arr;
