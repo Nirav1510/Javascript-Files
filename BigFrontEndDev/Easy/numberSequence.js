6aa08a3319719e2b169718e0f57e70eefcca8800
Nirav Gusai <gusainirav1@gmail.com>
2024-12-04 22:40:19 +0530

    [Big Frontend] getNthNum

---
+++ BigFrontEndDev/Easy/numberSequence.js
@@ -0,0 +1,31 @@
+const transform = (num) => {
+  let currentDigit = "";
+  let result = "";
+  let currentCount = 0;
+
+  for (let i = 0; i <= num.length; i++) {
+    if (num[i] === currentDigit) {
+      currentCount += 1;
+    } else {
+      if (currentCount) {
+        result += currentCount + currentDigit;
+      }
+      currentDigit = num[i];
+      currentCount = 1;
+    }
+  }
+  return result;
+};
+
+const getNthNum = (n) => {
+  let num = "1";
+
+  while (n > 1) {
+    num = transform(num);
+    n--;
+  }
+
+  return num;
+};
+
+console.log(getNthNum(5));
