83d1b4c139a7fb9bb97f804e5d452e88d79eb772
Nirav Gusai <gusainirav1@gmail.com>
2024-12-15 19:10:47 +0530

    update

--- Polyfills/groupBy.js
+++ Polyfills/groupBy.js
@@ -21,3 +21,12 @@ const a = myGroupBy([6.1, 4.2, 6.3], Math.floor);
 console.log("a", a);
 const b = myGroupBy(["one", "two", "three"], "length");
 console.log("b", b);
+const c = myGroupBy(
+  [
+    { user: "barney", age: 36, active: true },
+    { user: "fred", age: 40, active: false },
+    { user: "pebbles", age: 1, active: true },
+  ],
+  "active"
+);
+console.log("c", c);
