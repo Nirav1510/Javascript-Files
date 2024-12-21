c5e45b31d1b4e4ea464d27de6dbd26452d1032e0
Nirav Gusai <gusainirav1@gmail.com>
2024-12-21 13:37:14 +0530

    update

--- BigFrontEndDev/Easy/countBits.js
+++ BigFrontEndDev/Easy/countBits.js
@@ -1,12 +1,13 @@
 // https://bigfrontend.dev/problem/how-many-1s-in-binary
 
-function countBits(n) {
+const countBits = (n) => {
   let count = 0;
+
   while (n) {
     count += n & 1;
     n = n >> 1;
   }
   return count;
-}
+};
 
 console.log(countBits(10));
