c16f3ca767f797db988761298f5f225f3dd424c9
Nirav Gusai <gusainirav1@gmail.com>
2025-01-21 20:33:34 +0530

    permutationWithCaseChange

---
+++ Practice/easy/permutationWithCaseChange.js
@@ -0,0 +1,21 @@
+const solve = (ans, ip, op, idx) => {
+  if (idx === ip.length) {
+    ans.push(op);
+    return;
+  }
+
+  let op1 = op + ip[idx];
+  let op2 = op + ip[idx].toUpperCase();
+
+  solve(ans, ip, op1, idx + 1);
+  solve(ans, ip, op2, idx + 1);
+};
+
+const permutationWithCaseChange = (s) => {
+  let ans = [];
+  let op = "";
+
+  solve(ans, s, op, 0);
+  console.log("ans", ans);
+  return ans;
+};
