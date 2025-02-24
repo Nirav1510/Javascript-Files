6367618915f57c75cb3ad8ab9a4ae905e9765879
Nirav Gusai <gusainirav1@gmail.com>
2025-02-24 09:57:28 +0530

    myGroupByReduce

--- Polyfills/groupBy.js
+++ Polyfills/groupBy.js
@@ -14,6 +14,20 @@ const myGroupBy = (values, keySelector) => {
 	return ans;
 };
 
+const myGroupByReduce = (values, keySelector) => {
+	return values.reduce((acc, value) => {
+		const key = typeof keySelector === 'function' ? keySelector(value) : value[keySelector];
+
+		if (!acc[key]) {
+			acc[key] = [value];
+		} else {
+			acc[key].push(value);
+		}
+
+		return acc;
+	}, {});
+};
+
 const a = myGroupBy([6.1, 4.2, 6.3], Math.floor);
 console.log('a', a);
 const b = myGroupBy(['one', 'two', 'three'], 'length');
